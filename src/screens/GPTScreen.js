7704f6dc44bfa8ece1da91b7d9cae8c118b4bda0
JunHeeKim28 <t0pic13@naver.com>
2023-11-09 20:23:55 +0900

    feat: GPT디자인

--- src/screens/GPTScreen.js
+++ src/screens/GPTScreen.js
@@ -1,5 +1,15 @@
 import React, {useState} from 'react';
-import {View, Text, TextInput, Button, ScrollView} from 'react-native';
+import {
+  View,
+  Text,
+  TextInput,
+  Button,
+  ScrollView,
+  StyleSheet,
+  TouchableOpacity,
+  ImageBackground,
+  Image,
+} from 'react-native';
 import axios from 'axios';
 
 const GPTScreen = () => {
@@ -36,36 +46,82 @@ const GPTScreen = () => {
   };
 
   return (
-    <View style={{padding: 20}}>
-      <ScrollView style={{height: '80%'}}>
+    <View style={styles.container}>
+      <ScrollView style={styles.scrollView}>
         {chat.map((message, index) => (
-          <View key={index} style={{marginVertical: 5}}>
-            <Text
-              style={{
+          <View
+            key={index}
+            style={[
+              styles.messageContainer,
+              {
                 alignSelf: message.role === 'user' ? 'flex-end' : 'flex-start',
-              }}>
-              {message.message}
-            </Text>
+                backgroundColor:
+                  message.role === 'user' ? 'orange' : 'lightblue',
+              },
+            ]}>
+            <Text style={{color: '#000'}}>{message.message}</Text>
           </View>
         ))}
       </ScrollView>
-      <TextInput
-        style={{
-          height: 40,
-          borderColor: 'gray',
-          borderWidth: 1,
-          marginBottom: 10,
-        }}
-        onChangeText={text => setUserInput(text)}
-        value={userInput}
-      />
-      <Button
-        title={loading ? 'Loading...' : 'Send Request'}
-        onPress={sendMessage}
-        disabled={loading}
-      />
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.textInput}
+          onChangeText={text => setUserInput(text)}
+          value={userInput}
+          placeholder="메시지를 입력하세요..."
+        />
+        <TouchableOpacity
+          style={styles.sendBtn}
+          onPress={sendMessage}
+          disabled={loading}>
+          <Text style={styles.sendBtnTxt}>
+            {loading ? '로딩 중...' : '전송'}
+          </Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
-
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: '#000',
+  },
+  scrollView: {
+    flex: 1,
+    marginBottom: 10,
+  },
+  messageContainer: {
+    borderRadius: 8,
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    marginVertical: 5,
+    maxWidth: '70%',
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  textInput: {
+    flex: 1,
+    height: 40,
+    borderColor: '#ccc',
+    borderWidth: 1,
+    borderRadius: 10,
+    paddingHorizontal: 15,
+    marginRight: 10,
+    backgroundColor: '#fff',
+  },
+  sendBtn: {
+    backgroundColor: '#be289d',
+    borderRadius: 10,
+    paddingVertical: 10,
+    paddingHorizontal: 15,
+  },
+  sendBtnTxt: {
+    color: 'white',
+    fontSize: 16,
+  },
+});
 export default GPTScreen;
